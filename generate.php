bd55e9f8c5f2e76623b9cd7d48fc79a4ca08ca12
echovick <uchechukwueze70@gmail.com>
2025-03-03 21:26:05 +0100

    Add HTML Purifier and ZipStream-PHP libraries with initial configuration files

--- generate.php
+++ generate.php
@@ -1,52 +1,58 @@
 <?php
 require 'vendor/autoload.php';
 
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpWord\PhpWord;
-use PhpOffice\PhpWord\Shared\Html;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+use PhpOffice\PhpWord\TemplateProcessor;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Get uploaded files
-    $wordFile = $_FILES['wordFile']['tmp_name'];
+    $wordFile  = $_FILES['wordFile']['tmp_name'];
     $excelFile = $_FILES['excelFile']['tmp_name'];
 
     // Load the Excel file
-    $spreadsheet = IOFactory::load($excelFile);
-    $sheet = $spreadsheet->getActiveSheet();
+    $reader = new Xlsx();
+    $reader->setReadDataOnly(true);
+    $spreadsheet = $reader->load($excelFile);
+    $sheet       = $spreadsheet->getActiveSheet();
 
-    // Extract data from Excel
-    $data = [];
-    foreach ($sheet->getRowIterator() as $row) {
+    $rowCount = 0;
+    foreach ($sheet->getRowIterator() as $index => $row) {
+        if ($rowCount >= 5) {
+            break;
+        }
+
+        if ($index == 0) {
+            continue;
+        }
+
         $cellIterator = $row->getCellIterator();
         $cellIterator->setIterateOnlyExistingCells(false);
         $rowData = [];
         foreach ($cellIterator as $cell) {
             $rowData[] = $cell->getValue();
         }
-        $data[] = $rowData;
+
+        if (is_string($rowData[0])) {
+            continue;
+        }
+
+        // Create a new TemplateProcessor instance for each row
+        $templateProcessor = new TemplateProcessor($wordFile);
+        $templateProcessor->setValue('Sno', $rowData[0] ?? '');
+        $templateProcessor->setValue('AgentBroker_Name', $rowData[7] ?? '');
+        $templateProcessor->setValue('Address', $rowData[14] ?? '');
+        $templateProcessor->setValue('Insured_Name', $rowData[4] ?? '');
+        $templateProcessor->setValue('Policy_No', $rowData[3] ?? '');
+        $templateProcessor->setValue('Expiry_Date', $rowData[22] ?? '');
+        $templateProcessor->setValue('SumInsured', $rowData[17] ?? '');
+        $templateProcessor->setValue('Gross_Premium', $rowData[18] ?? '');
+        $templateProcessor->setValue('Class__of_Business', $rowData[10] ?? '');
+
+        // Generate a unique filename for each document
+        $outputFile = 'filled_template_' . ($rowCount + 1) . '.docx';
+        $templateProcessor->saveAs($outputFile);
+
+        echo "Generated: $outputFile <br>";
+        $rowCount++;
     }
-
-    // Load the Word template
-    $phpWord = new PhpWord();
-    $templateProcessor = $phpWord->loadTemplate($wordFile);
-
-    // Fill the Word template with data
-    foreach ($data as $index => $row) {
-        $templateProcessor->setValue('Sno#' . ($index + 1), $row[0]);
-        $templateProcessor->setValue('AgentBroker_Name#' . ($index + 1), $row[1]);
-        $templateProcessor->setValue('Address#' . ($index + 1), $row[2]);
-        $templateProcessor->setValue('Insured_Name#' . ($index + 1), $row[3]);
-        $templateProcessor->setValue('Policy_No#' . ($index + 1), $row[4]);
-        $templateProcessor->setValue('Expiry_Date#' . ($index + 1), $row[5]);
-        $templateProcessor->setValue('SumInsured#' . ($index + 1), $row[6]);
-        $templateProcessor->setValue('Gross_Premium#' . ($index + 1), $row[7]);
-        $templateProcessor->setValue('Class__of_Business#' . ($index + 1), $row[8]);
-    }
-
-    // Save the filled Word document
-    $outputFile = 'filled_template.docx';
-    $templateProcessor->saveAs($outputFile);
-
-    echo "Word document has been filled and saved as '$outputFile'.";
 }
-?>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,22 +6,86 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit175f515b2811e085f3258b59792b25af
 {
+    public static $files = array (
+        '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'Z' => 
+        array (
+            'ZipStream\\' => 10,
+        ),
         'P' => 
         array (
+            'Psr\\SimpleCache\\' => 16,
+            'Psr\\Http\\Message\\' => 17,
+            'Psr\\Http\\Client\\' => 16,
             'PhpOffice\\PhpWord\\' => 18,
+            'PhpOffice\\PhpSpreadsheet\\' => 25,
             'PhpOffice\\Math\\' => 15,
+        ),
+        'M' => 
+        array (
+            'Matrix\\' => 7,
+        ),
+        'C' => 
+        array (
+            'Composer\\Pcre\\' => 14,
+            'Complex\\' => 8,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'ZipStream\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/maennchen/zipstream-php/src',
+        ),
+        'Psr\\SimpleCache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/simple-cache/src',
+        ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'Psr\\Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-client/src',
+        ),
         'PhpOffice\\PhpWord\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpoffice/phpword/src/PhpWord',
         ),
+        'PhpOffice\\PhpSpreadsheet\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet',
+        ),
         'PhpOffice\\Math\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpoffice/math/src/Math',
+        ),
+        'Matrix\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/matrix/classes/src',
+        ),
+        'Composer\\Pcre\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/composer/pcre/src',
+        ),
+        'Complex\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/complex/classes/src',
+        ),
+    );
+
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'HTMLPurifier' => 
+            array (
+                0 => __DIR__ . '/..' . '/ezyang/htmlpurifier/library',
+            ),
         ),
     );
 
@@ -34,6 +98,7 @@ class ComposerStaticInit175f515b2811e085f3258b59792b25af
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit175f515b2811e085f3258b59792b25af::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit175f515b2811e085f3258b59792b25af::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit175f515b2811e085f3258b59792b25af::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit175f515b2811e085f3258b59792b25af::$classMap;
 
         }, null, ClassLoader::class);
